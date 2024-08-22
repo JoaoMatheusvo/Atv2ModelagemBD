6587b5e8eaeac74e1654d95f503978333fb5f727
João Matheus <127622280+JoaoMatheusvo@users.noreply.github.com>
2024-08-21 21:21:54 -0300

    Update ConexaoBD.java

--- ConexaoBD.java
+++ ConexaoBD.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConexaoBD {
     private static ConexaoBD instancia;
-    private static final String URL = "jdbc:mysql://localhost:3306/javasqlbd";
+    private static final String URL = "jdbc:mysql://localhost:3306/javasqlbd2";
     private static final String USER = "root";
     private static final String PASSWORD = "12345";
 
